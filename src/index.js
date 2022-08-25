288831c38daab9f3e366d8654163bff6d856184d
Palash Mandal <palash15-2092@diu.edu.bd>
2022-08-25 14:36:05 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+require('./db');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Track Map API');
 });
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!');
+app.listen(port, () => {
+  console.log(`Listening on port ${port}.`);
 });
